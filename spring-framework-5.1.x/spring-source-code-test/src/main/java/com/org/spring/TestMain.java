8bc7ebc4ba9a4b5f6b02c47638494fac9e1a7f1a
574876372@qq.com <574876372@qq.com>
2020-06-22 11:06:42 +0800

    源码处理

--- spring-framework-5.1.x/spring-source-code-test/src/main/java/com/org/spring/TestMain.java
+++ spring-framework-5.1.x/spring-source-code-test/src/main/java/com/org/spring/TestMain.java
@@ -2,6 +2,7 @@ package com.org.spring;
 
 import com.org.spring.app.AppConfig;
 import com.org.spring.service.CityService;
+import com.org.spring.service.OrderService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestMain {
@@ -10,7 +11,9 @@ public class TestMain {
 
 		// 初始化 spring 上下文
 		AnnotationConfigApplicationContext  ac = new AnnotationConfigApplicationContext(AppConfig.class);
-		System.out.println(ac.getBean(CityService.class));
+		OrderService o1 =(OrderService) ac.getBean("orderService");
+		OrderService o2 =(OrderService) ac.getBean("orderService");
+		System.out.println(o1==o2);
 	}
 
 }
---
+++ spring-framework-5.1.x/spring-source-code-test/src/main/java/com/org/spring/spring/LuBanBeanFactroyPostProcessor.java
@@ -0,0 +1,17 @@
+package com.org.spring.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LuBanBeanFactroyPostProcessor implements BeanFactoryPostProcessor {
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		BeanDefinition beanDefinition = beanFactory.getBeanDefinition("orderService");
+		beanDefinition.setScope("singleton");
+	}
+}
